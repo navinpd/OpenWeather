cd939c8795be424e236c323a1dfd1df21294dead
navinpd <10051278@gemalto.com>
2020-10-24 13:31:10 +0800

    Added certificate pinning. Pending caching & UI beautification

--- app/src/main/java/com/big/openweather/data/remote/Networking.kt
+++ app/src/main/java/com/big/openweather/data/remote/Networking.kt
@@ -3,6 +3,7 @@ package com.big.openweather.data.remote
 
 import com.big.openweather.BuildConfig
 import okhttp3.Cache
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -15,29 +16,33 @@ object Networking {
 
 
     fun createNetworking(
-            apiVal: String,
-            baseUrl: String,
-            cascheDir: File,
-            cascheSize: Long
+        apiVal: String,
+        baseUrl: String,
+        cascheDir: File,
+        cascheSize: Long
     ): NetworkService {
         API_VAL = apiVal
         return Retrofit.Builder()
-                .baseUrl(baseUrl)
-                .client(
-                        OkHttpClient.Builder()
-                                .cache(Cache(cascheDir, cascheSize))
-                                .addInterceptor(HttpLoggingInterceptor()
-                                        .apply {
-                                            level = if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY
-                                            else HttpLoggingInterceptor.Level.NONE
-                                        })
-                                .build()
-                )
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-                .create(
-                        NetworkService::class.java
-                )
+            .baseUrl(baseUrl)
+            .client(
+                OkHttpClient.Builder()
+                    .cache(Cache(cascheDir, cascheSize))
+                    .addInterceptor(HttpLoggingInterceptor()
+                        .apply {
+                            level = if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY
+                            else HttpLoggingInterceptor.Level.NONE
+                        })
+                        .certificatePinner(CertificatePinner.Builder()
+                                .add("*openweathermap.org/*",
+                                        "sha256/51ec81c0ace1dffd11ec631469cb3da27ea79fe15923c25f3a50a7f8642b2b79")
+                        .build())
+                    .build()
+            )
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(
+                NetworkService::class.java
+            )
     }
 
 }
